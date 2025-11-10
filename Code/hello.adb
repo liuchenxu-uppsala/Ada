a155b58133b69833ab468a8471c20360aff41459
liuchenxu-uppsala <chenxu.l@outlook.com>
2025-11-10 20:52:21 +0100

    Hello Ada

---
+++ Code/hello.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO;
+
+procedure Hello is
+begin
+	-- Print "Hello World!" on the display
+	Ada.Text_IO.Put_Line("Hello World!");
+end Hello;
---
+++ Hello/src/main.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO;
+procedure Main is
+
+begin
+   Ada.Text_IO.Put_Line ("Hello, World!");
+   Ada.Text_IO.Put_Line ("Hi Ada");
+   null;
+end Main;
